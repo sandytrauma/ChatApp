fc982c25f5c6ac5389d9765845adca705f8c01c8
sandytrauma <sksandeep443gmail.com>
2023-11-11 13:03:28 +0530

    chatApp functionality login access and hide chat

--- ChatApp/script.js
+++ ChatApp/script.js
@@ -1,115 +1,85 @@
-// Get DOM elements
+// Global variables
+const messages = []; // Array to store chat messages
+let username = '';
 
-const usernameInput = document.getElementById('username-input');
+let loggedIn = false;
 
-const messageInput = document.getElementById('message-input');
+// Function to handle user login
+function handleLogin(event) {
+  event.preventDefault(); // Prevent form submission
 
-const sendButton = document.getElementById('send-button');
+  const usernameInput = document.getElementById('username');
+  username = usernameInput.value;
+  
+  alert(`Welcome, ${username}!`);
 
-const resetbutton = document.getElementById('reset-button')
+  loggedIn = true;
 
-const chatMessages = document.getElementById('chat-messages');
-
-
-
-
-//Array to store messages
-let messages =[];
-
-console.log(messages)
-
-
-//load cached message on page load
-window.addEventListener('load',loadCachedMessages);
-
-//Event listener for send button click
-sendButton.addEventListener('click', sendMessage);
-
-//Event listener for enter key press in message input
-messageInput.addEventListener('keypress', function (e){
-    if (e.key==='Enter'){
-        sendMessage();
-    }
-});
-
-//Function to send a message
-function sendMessage(){
-    const username = usernameInput.value;
-    const message = messageInput.value;
-    if (username.trim()!=='' && message.trim()!==''){
-        //Add message to array
-        messages.push({username,message});
-
-        //Create message element and append to chat container
-        const messageElement = document.createElement('p');
-        
-        messageElement.classList.add('message');
-        messageElement.textContent = `${username}:${message}`;
-        chatMessages.appendChild(messageElement);
-
-        document.getElementById("myDIV").appendChild(messageElement);
-
-        //Clear input field
-        usernameInput.value = '';
-        messageInput.value = '';
-
-        //message cache
-        cacheMessage({username,message});
-    } 
-    
+  usernameInput.value = '';
+  document.getElementById('username').textContent = username;
+  
+  
+  document.getElementById('login-container').style.display = 'none'; // Hide login section  
+  document.getElementById('chat-message-container').style.display = 'block'; // Show chat container
+  document.getElementById('chat-messages').style.display = 'block'; // Show chat section
+  
 }
 
-function cacheMessage(message){
-    //Initialize an empty array
-    const cachedMessages = JSON.parse(localStorage.getItem('chatMessages'))||[];
+// Function to handle chat input
+function handleChatInput(event) {
+  event.preventDefault(); // Prevent form submission
 
-    //Add the new message to the array
-    cachedMessages.push(message);
+  if(!loggedIn){
+    alert("Please login before sending messages!");
+    return;
+  }
 
-    //Store the updated array in local storage
-    localStorage.setItem('chatMessages',JSON.stringify(cachedMessages));
+  const messageInput = document.getElementById('message-input');
+  const message = messageInput.value;
+
+  if (message.trim() !== '') {
+    sendMessage(message); // Call the sendMessage function
+
+    messageInput.value = ''; // Clear the input field
+  }
 }
 
-function loadCachedMessages(){
-    //Get cached message from local storage
-    const cachedMessages = JSON.parse(localStorage.getItem('chatMessages'));
+// Function to send message
+function sendMessage(message) {
+  const timestamp = new Date().toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  messages.push({ message, timestamp }); // Store the message and its timestamp
 
-    if(cachedMessages && cachedMessages.length>0){
-
-        cachedMessages.forEach((message)=>{
-            const messageElement = document.createElement('p');
-            messageElement.textContent = `${message.username}:${message.message}`;
-            chatMessages.appendChild(messageElement);
-            document.getElementById("myDIV").appendChild(messageElement);
-            messageElement.style.color ='#021f3e';
-            messageElement.style.fontFamily ='Nunito';
-            messageElement.style.fontWeight ='800';
-            messageElement.style.fontSize ='1.1rem';         
-           
-        });
-
-        
-    }
+  appendMessage(message, timestamp, username); // Call the appendMessage function
 }
 
-resetbutton.addEventListener('click',resetChat);
-function resetChat(){
-    chatMessages.innerHTML='';
-
-    messages =[];
-
-    localStorage.removeItem('chatMessages');
-
-    alert('Chat has been reset!');
-   
+// Function to append message to chat messages element
+function appendMessage(message, timestamp, username) {
+  const chatMessages = document.getElementById('chat-messages');
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = `<span class="timestamp float-end">${timestamp}</span><span class="m-2 text-uppercase fs-5 fw-bold">${username+':'}</span>${message}`;
+  chatMessages.appendChild(messageElement);
 }
 
+// Function to clear chat messages
+function clearChat() {
+  const chatMessages = document.getElementById('chat-messages');
+  chatMessages.innerHTML = '';
+  messages.length = 0; // Clear the messages array
+}
 
+// Function to cache messages
+function cacheMessages() {
+  localStorage.setItem('cachedMessages', JSON.stringify(messages));
+}
 
+// Event listener for login form submission
+document.getElementById('login-form').addEventListener('submit', handleLogin);
 
+// Event listener for chat form submission
+document.getElementById('chat-form').addEventListener('submit', handleChatInput);
 
-
-
-
-
-
+// Event listener for clear button click
+document.getElementById('clear-button').addEventListener('click', clearChat);
