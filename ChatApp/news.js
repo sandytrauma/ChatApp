5d6273971382d99bc13ee51842177556cf4b9348
sandytrauma <sksandeep443gmail.com>
2023-11-29 13:54:47 +0530

    news item update

--- ChatApp/news.js
+++ ChatApp/news.js
@@ -6,6 +6,31 @@ async function fetchData(query){
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`) 
     const data = await res.json()
     console.log(data)
+    displayNews(data.articles);
 }
 
-fetchData("everything")
+function displayNews(articles){
+    const newsContainer = document.getElementById("news-container");
+
+    articles.forEach(article =>{
+        const newsItem = document.createElement("div");
+        newsItem.classList.add("news-item");
+
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+  
+        const description = document.createElement("p");
+        description.textContent = article.description;
+  
+        const source = document.createElement("p");
+        source.textContent = `Source:${article.source.name}`;
+  
+        newsItem.appendChild(title);
+        newsItem.appendChild(description);
+        newsItem.appendChild(source);
+  
+        newsContainer.appendChild(newsItem);
+    })
+}
+
+fetchData("India")
